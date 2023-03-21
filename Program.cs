f01302abca62fb7a652a631c69937f9e6da81530
Ales Krivoruchko <jeremieua@gmail.com>
2023-03-21 22:14:16 +0300

    made 1st method for take size of output array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int SizeOfOutputArrayIfLess4(string[] arr)
+{
+    int size = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            size++;
+        }
+    }
+    return size;
+}
+
+string[] incomingarray = Console.ReadLine()!.Split(' ');
+
+int size = SizeOfOutputArrayIfLess4(incomingarray);
+
+Console.Write(size);
